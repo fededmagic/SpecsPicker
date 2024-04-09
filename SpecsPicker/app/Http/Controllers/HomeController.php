41138671dc2dd0b949bc7acff2b03f606e6dd878
Thomas Pashollari (External) <E-thomas.pashollari@msc.com>
2024-04-09 15:18:55 +0200

    Inizio sviluppo interfaccia di deault

--- SpecsPicker/app/Http/Controllers/HomeController.php
+++ SpecsPicker/app/Http/Controllers/HomeController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
         $viewData = [];
         $viewData['title'] = 'SpecsPicker';
         $viewData['subtitle'] = 'Search the minimum hardware specs for a software!';
+        $viewData['result'] = "";
 
+        $request->validate([
+            'txtInput' => "required | max:255",
+        ]);
+
+        $viewData = $request->input('txtInput');
         return view('home.index')->with("viewData", $viewData);
     }
 }

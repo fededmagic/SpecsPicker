33c97774acd6d9b52fb3cce98a166583b5a10cf4
Federico Dutto <federico.dutto@itiscuneo.eu>
2024-04-13 20:30:57 +0200

    Prima versione della risposta dell'ai

--- SpecsPicker/app/Http/Controllers/HomeController.php
+++ SpecsPicker/app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+//require 'vendor/autoload.php';
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
@@ -28,7 +30,47 @@ class HomeController extends Controller
             'txtInput' => "required | max:255"
         ]);
 
-        $viewData["result"] = $request->input('txtInput');
+        $inputSoftware = $request->input('txtInput');
+
+        $viewData["result"] = HomeController::apiRequest($inputSoftware);
         return view('home.index')->with("viewData", $viewData);
+    }
+
+    private static function apiRequest($inputSoftware) {
+
+        $client = new \GuzzleHttp\Client();
+        $client = new Client([
+            'verify' => false
+        ]); //disabilita il certificato SSL (non raccomandabile per l'ambiente di produzione)
+
+        $response = HomeController::makeRequest($client, $inputSoftware);
+
+        return $response->getBody();
+    }
+
+    private static function makeRequest($client, $inputSoftware) {
+
+        return $client->request('POST', 'https://chatgpt-42.p.rapidapi.com/conversationgpt4', [
+            'body' => '{
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "give me a json without \n with minumun and suggested hardware specifications of ' 
+                    . $inputSoftware . '"
+                }
+            ],
+            "system_prompt": "",
+            "temperature": 0.9,
+            "top_k": 5,
+            "top_p": 0.9,
+            "max_tokens": 256,
+            "web_access": false
+        }',
+            'headers' => [
+                'X-RapidAPI-Host' => 'chatgpt-42.p.rapidapi.com',
+                'X-RapidAPI-Key' => '4ac7ad12edmsh68893b14d13e403p13b433jsnb8329201aca7',
+                'content-type' => 'application/json',
+            ],
+        ]);
     }
 }

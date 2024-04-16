f4404f4e26b2a0530a963e00a11897b7a952046e
Federico Dutto <federico.dutto@itiscuneo.eu>
2024-04-16 15:28:13 +0200

    Start json formatting

--- SpecsPicker/app/Http/Controllers/HomeController.php
+++ SpecsPicker/app/Http/Controllers/HomeController.php
@@ -8,6 +8,9 @@ use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
+    public static string $message = "give me a json formatted with the fields
+        Operating system, CPU, Memory, GPU, Storage with minumun and 
+        suggested hardware specifications of ";
 
     public function index()
     {
@@ -45,7 +48,7 @@ class HomeController extends Controller
 
         $response = HomeController::makeRequest($client, $inputSoftware);
 
-        return $response->getBody();
+        return HomeController::parseBody($response);
     }
 
     private static function makeRequest($client, $inputSoftware) {
@@ -55,8 +58,7 @@ class HomeController extends Controller
             "messages": [
                 {
                     "role": "user",
-                    "content": "give me a json without \n with minumun and suggested hardware specifications of ' 
-                    . $inputSoftware . '"
+                    "content": "give me a json formatted with only the fields Operating system, CPU, Memory, GPU, Storage with minumun and suggested hardware specifications of' .  $inputSoftware . '"
                 }
             ],
             "system_prompt": "",
@@ -71,6 +73,19 @@ class HomeController extends Controller
                 'X-RapidAPI-Key' => '4ac7ad12edmsh68893b14d13e403p13b433jsnb8329201aca7',
                 'content-type' => 'application/json',
             ],
-        ]);
+        ])->getBody();
+    }
+
+    private static function makeFakeRequest($client, $inputSoftware) {
+
+        return '{"result":"Here is the JSON format for The Last of Us hardware specifications:```json{    \"Operating System\": {        \"Minimum\": \"Windows 7 (64-bit)\",        \"Recommended\": \"Windows 10 (64-bit)\"    },    \"CPU\": {        \"Minimum\": \"Intel Core i3-560 @ 3.3GHz or AMD Phenom II X4 945\",        \"Recommended\": \"Intel Core i5-2500K @ 3.3GHz or AMD FX-8320 @ 3.5GHz\"    },    \"Memory\": {        \"Minimum\": \"4 GB RAM\",        \"Recommended\": \"8 GB RAM\"    },    \"GPU\": {        \"Minimum\": \"NVIDIA GeForce GTX 660 / AMD Radeon HD 7870 (2GB VRAM)\",        \"Recommended\": \"NVIDIA GeForce GTX 970 (4GB) / AMD Radeon R","status":true,"server_code":1}';
+    }
+
+    public static function parseBody($inputString): string {
+
+        $inputString = str_replace('\ ', '', $inputString);
+        $inputString = str_replace('\n', '', $inputString);
+
+        return $inputString;
     }
 }
